4948777fd21df7d4a203ec44e1cb150a67c3dc73
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-10 13:30:32 -0400

    Rendered the hero component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Grid from "./components/Grid";
+import Hero from "./components/Hero";
 
 function App() {
 
   return (
     <Layout>
       <main>
+        <Hero />
         <Grid />
       </main>
     </Layout>
