fe6648efc380928cc8ca5ffc6c3802d856d1d731
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-10 13:30:17 -0400

    Added the hero section as a new component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <div>
+            <h5>Complete this training program if you want to...</h5>
+            <ol className="benefits-list">
+                <li>Follow a simple program with guaranteed results</li>
+                <li>Get fit, healthy, strong and shredded</li>
+                <li>Learn more about gym, training and technique</li>
+                <li>Become a lifetime gym bro 😎</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Hero;
