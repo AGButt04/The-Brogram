337a988f1b7700291960fc01c715e21c151629fe
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-10 13:44:51 -0400

    Finished laying out the Hero Section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Hero() {
     return (
         <div>
+
             <h5>Complete this training program if you want to...</h5>
             <ol className="benefits-list">
                 <li>Follow a simple program with guaranteed results</li>
@@ -10,6 +11,48 @@ function Hero() {
                 <li>Learn more about gym, training and technique</li>
                 <li>Become a lifetime gym bro 😎</li>
             </ol>
+            <h3>The Rules</h3>
+            <p>To complete this program, you <b>MUST</b> follow these 3 simple rules:</p>
+            <ul className="rules-list">
+                <div className="rule-item">
+                    <p><b>Rest</b></p>
+                    <p>Ensure that you are taking rest days where necessary</p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Reps</b></p>
+                    <p>Ensure that you are taking rest days where necessary</p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Rest</b></p>
+                    <p>Every rep is a pause rep following 
+                        <abbr title="2 seconds down - 2 seconds pause - 2 seconds up"> 2 - 2 - 2 tempo</abbr>
+                    </p>
+                </div>
+                 <div className="rule-item">
+                    <p><b>*Weight*</b></p>
+                    <p>
+                        Select the maximum weight that allows you to
+                        complete the set with the good form.
+                    </p>
+                </div>
+            </ul>
+            <small>
+                * The first and second set should be at 75% and 85% of 
+                your working weight used for the last two sets.
+            </small>
+            <h3>The Training Plan</h3>
+            <p>
+                The training plan uses a structure known as <b>Bro Split</b>
+                , and follows this rotation ⬇️.
+            </p>
+            <p>
+                <b>
+                    <i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i>
+                </b>
+            </p>
+            <p>
+                Complete all of the workouts below and track your progress ✅.
+            </p>
         </div>
     )
 }
