0fcb58050392dbb10a29067d92b4476b39a4fa2e
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-11 12:29:04 -0400

    Modal component created

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import ReactDom from "react-dom";
 
-function Modal() {
-    return (
-        <></>
-    )
+function Modal(props) {
+    const { showExerciseDescription, handleCLoseModal } = props;
+    const { name, description } = showExerciseDescription || {};
+
+    return ReactDom.createPortal((
+        <div className="modal-container">
+            <button className="modal-underlay" onClick={handleCLoseModal}/>
+            <div className="modal-container">
+                <div>
+                    <h6>Name</h6>
+                    <h2 className="skill-name">{name.replaceAll('-', ' ')}</h2>
+                </div>
+                <div>
+                    <h6>Description</h6>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    ), document.getElementById("portal"));
 }
 
 export default Modal;
