d55e22bdc4d6cd4be99e9e049e3f21d8a7008768
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-11 15:50:58 -0400

    Added the modal logic as a component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,13 @@
 import ReactDom from "react-dom";
 
 function Modal(props) {
-    const { showExerciseDescription, handleCLoseModal } = props;
+    const { showExerciseDescription, handleCloseModal } = props;
     const { name, description } = showExerciseDescription || {};
 
     return ReactDom.createPortal((
         <div className="modal-container">
-            <button className="modal-underlay" onClick={handleCLoseModal}/>
-            <div className="modal-container">
+            <button className="modal-underlay" onClick={handleCloseModal}/>
+            <div className="modal-content">
                 <div>
                     <h6>Name</h6>
                     <h2 className="skill-name">{name.replaceAll('-', ' ')}</h2>
